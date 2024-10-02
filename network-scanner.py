9793d13102aebaff5ceb0a06b504fe918864658f
ved patel <pved81034@gmail.com>
2024-10-02 16:19:26 +0530

    create network scanner

--- network-scanner.py
+++ network-scanner.py
@@ -1,7 +1,18 @@
 import scapy.all as scapy
 
+# def scanner(ip):
+#       scapy.arping(ip)
+
 def scanner(ip):
-      scapy.arping(ip)
+      request = scapy.ARP(pdst=ip)
+      broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+      merge = request/broadcast
+      answered_list = scapy.srp(merge,timeout=1,verbose=False)[0]
+      unanswered_list = scapy.srp(merge,timeout=1,verbose=False)[1]
+
+      for i in answered_list:
+            print(i)
+            print("-----------")
 
 
 scanner("192.168.56.1/24")
